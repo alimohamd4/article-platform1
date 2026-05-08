be47cd584178f7d05873c3ab561dd2c4e1e5925d
alimohamd4 <ali0932502889ali@gmail.com>
2026-05-08 15:33:30 +0100

    Fix WebSocket JWT auth middleware

--- config/asgi.py
+++ config/asgi.py
@@ -1,14 +1,20 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from config.routing import websocket_urlpatterns
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from apps.notify.middleware import JWTAuthMiddleware
+from config.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+    'websocket': AllowedHostsOriginValidator(
+        JWTAuthMiddleware(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
 })
